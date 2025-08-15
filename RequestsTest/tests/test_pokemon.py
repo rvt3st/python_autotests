78e8139c7e6316fb4b4637dd2b6f924f4a61aab1
Roman <valkov.r.a@gmail.com>
2025-08-15 17:35:31 +0700

    Update test_pokemon.py

--- RequestsTest/tests/test_pokemon.py
+++ RequestsTest/tests/test_pokemon.py
@@ -6,7 +6,7 @@ token = 'USER_TOKEN'
 headers = {'Content-Type' : 'application/json', 'trainer_token' : token}
 trainer_id = 38235
 
-# 1. Проверка, что ответ запрос GET /trainers приходит с кодом 200
+# 1. Проверка, что ответ на запрос GET /trainers приходит с кодом 200
 
 def test_status_code():
 	response = requests.get(url = f'{host}/trainers')
@@ -18,3 +18,4 @@ def test_status_code():
 def test_trainerName():
 	response_trainer = requests.get(url = f'{host}/trainers', params = {'trainer_id' : trainer_id})
 	assert "trainer_name" in response_trainer.json()["data"][0]
+
